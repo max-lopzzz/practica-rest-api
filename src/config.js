366c2becff5a41c5828ff70599daa5139f9ec2af
Maximiliano <m.lopz.montn@gmail.com>
2026-09-24 12:21:48 -0600

    Puerto por defecto 4000 en config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
   db: {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
